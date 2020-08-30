87ea425b1af7b44858f10539f7a2822f7f039295
Unknown <raphman100@gmail.com>
2020-08-30 17:33:04 -0400

    Added Google Firebase with sign in/out/auth

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+	apiKey: "AIzaSyBelRKrbw0HOIy-i-1qc7H4uSx9GCKV15Q",
+		authDomain: "rmr-crwn-clothing.firebaseapp.com",
+	databaseURL: "https://rmr-crwn-clothing.firebaseio.com",
+	projectId: "rmr-crwn-clothing",
+	storageBucket: "rmr-crwn-clothing.appspot.com",
+	messagingSenderId: "1048073305747",
+	appId: "1:1048073305747:web:0737e4c065bdd0b0099766",
+	measurementId: "G-R8T4XXE2JW"
+};
+
+firebase.initializeApp( config );
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters( { prompt: 'select_account' } );
+export const signInWithGoogle = () => auth.signInWithPopup( provider );
+
+export default firebase;
