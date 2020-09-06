aaf66b26d1798ea9d26407542d673c97e05f4709
Unknown <raphman100@gmail.com>
2020-09-06 18:00:35 -0400

    Added addItem and removeItem to checkout component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,9 +1,9 @@
 export const addItemToCart = ( cartItems, cartItemToAdd ) => {
-	const existingCartitem = cartItems.find(
+	const existingCartItem = cartItems.find(
 		cartItem => cartItem.id === cartItemToAdd.id
 	);
 
-	if ( existingCartitem ) {
+	if ( existingCartItem ) {
 		return cartItems.map( cartItem =>
 			cartItem.id === cartItemToAdd.id
 				? { ...cartItem, quantity: cartItem.quantity + 1 }
@@ -12,4 +12,20 @@ export const addItemToCart = ( cartItems, cartItemToAdd ) => {
 	}
 
 	return [ ...cartItems, { ...cartItemToAdd, quantity: 1 } ];
+}
+
+export const removeItemFromCart = ( cartItems, cartItemToRemove ) => {
+	const existingCartItem = cartItems.find(
+		cartItem => cartItem.id === cartItemToRemove.id
+	);
+
+	if ( existingCartItem.quantity === 1 ) {
+		return cartItems.filter( cartItem => cartItem.id !== cartItemToRemove.id );
+	}
+
+	return cartItems.map( cartItem =>
+		cartItem.id === cartItemToRemove.id
+			? { ...cartItem, quantity: cartItem.quantity - 1 }
+			: cartItem
+	);
 }
